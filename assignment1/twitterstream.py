68cc689b02f1e37365351f51adf8d0f4878c702a
Vladimir Iofik <v.iofik@gmail.com>
2016-04-02 20:16:11 +0300

    move secrets into separate file

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -3,10 +3,11 @@ import urllib2 as urllib
 
 # See assignment1.html instructions or README for how to get these credentials
 
-api_key = "<Enter api key>"
-api_secret = "<Enter api secret>"
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
+#api_key = "<Enter api key>"
+#api_secret = "<Enter api secret>"
+#access_token_key = "<Enter your access token key here>"
+#access_token_secret = "<Enter your access token secret here>"
+from twitter_secret import api_key, api_secret, access_token_key, access_token_secret
 
 _debug = 0
 
